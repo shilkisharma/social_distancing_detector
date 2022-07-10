d473628be7fb1c1adac8490c37d9e048ea9bcc73
shilkisharma <53752170+shilkisharma@users.noreply.github.com>
2022-07-10 20:53:10 +0530

    Add files via upload

---
+++ social_distancing_detector/packages/demo.py
@@ -0,0 +1,11 @@
+import pdb
+a=2
+b=3;
+print(a+b);
+a="56"
+for k in a:
+    a1=k+a1
+print(a)
+    
+
+
---
+++ social_distancing_detector/templates/assign_opr.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x=10;
+int y=20;
+x+=y;
+printf("\n value of x =%d",x);
+getch();
+}
